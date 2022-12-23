1fa99b3e54faa282bec47f04904d5bfa6930d6e6
Andres David Solarte Vidal <andresdavidsv@Andress-MacBook-Pro.local>
2022-12-23 18:10:44 -0500

    feature: Redux with middlewares

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
-import { pokemonsReducer } from './pokemons';
-import { uiReducer } from './ui';
+import dataReducer from '../slices/dataSlice';
+// import { pokemonsReducer } from './pokemons';
+// import { uiReducer } from './ui';
 
 const rootReducer = combineReducers({
-  data: pokemonsReducer,
-  ui: uiReducer,
+  data: dataReducer,
+  // ui: uiReducer,
 });
 
 export default rootReducer;
